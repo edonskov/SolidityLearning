9dfd8fd1c18dfdc8af1cb17554bb6c9f96b33b1a
edonskov <radion2002@gmail.com>
2021-01-15 10:16:30 +0300

    Bytes fixed

--- task1/SimpleStorageByte.sol
+++ task1/SimpleStorageByte.sol
@@ -2,13 +2,13 @@ pragma solidity >=0.7.0 <0.8.0;
 
 contract SimpleStorage {
     
-    string storedData;
+    bytes32 storedData;
 
-    function set(string memory _x) public {
+    function set(bytes32 _x) public {
         storedData = _x;
     }
 
-    function get() public view returns (string memory) {
+    function get() public view returns (bytes32) {
         return storedData;
     }
 }
