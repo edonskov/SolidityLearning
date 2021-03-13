003c3be012ecd282826e4a31859e3901affe0e04
edonskov@github.com <radion2002@gmail.com>
2021-03-14 00:52:20 +0300

    Task 14. Address library

---
+++ tasks/task14.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/utils/Address.sol";
+
+
+contract Task14 {
+    
+    using Address for address payable;
+    
+    address private owner;
+    uint public balance = 0;
+    
+    event Receive(
+        address indexed _from,
+        uint value
+    );
+    
+    event Send(
+        address indexed _from,
+        uint value
+    );
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function sendByOwner(address payable _receiver, uint value) public onlyOwner {
+        _receiver.isContract();
+        _receiver.sendValue(value);
+        balance -= value;
+        emit Send(msg.sender, value);
+    }    
+    
+    receive() external payable {
+        balance += msg.value;
+        emit Receive(msg.sender, msg.value);
+    }
+}
